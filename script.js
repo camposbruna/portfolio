8e8822dff4cd628c488bb90b2eba385dabe377b3
Perkles <camposjbruna@gmail.com>
2022-09-21 15:29:59 -0300

    section experience

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
-const btnMobile = document.getElementById('btn-mobile');
 
-function toggleMenu() {
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active');
 
+/* mudar o header da página quando der scroll */
+window.addEventListener('scroll', onScroll)
+
+function onScroll() {
+    let header = document.getElementById('header')
+    if(scrollY > 0) {
+        header.classList.add('scroll')
+    } else {
+        header.classList.remove('scroll')
+    }
+    }
+
+function abrirMenu() {
+    if (itens.style.display == 'block') {
+        itens.style.display = 'none'
+    } else {
+        itens.style.display = 'block'
+    }
 }
 
-btnMobile.addEventListener('click', toggleMenu);
+function mostrarListaAtividades1() {
+    let lista = document.querySelector('#emprego1')
+    if (lista.style.display == 'block') {
+        lista.style.display = 'none'
+    } else {
+        lista.style.display = 'block'
+    }
+}
+function mostrarListaAtividades2() {
+    let lista = document.querySelector('#emprego2')
+    if (lista.style.display == 'block') {
+        lista.style.display = 'none'
+    } else {
+        lista.style.display = 'block'
+    }
+}
+function mostrarListaAtividades3() {
+    let lista = document.querySelector('#emprego3')
+    if (lista.style.display == 'block') {
+        lista.style.display = 'none'
+    } else {
+        lista.style.display = 'block'
+    }
+}
+
